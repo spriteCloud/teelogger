02ef8ea72c693352bffff6b05c2277d5b10fb3a7
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-09-15 15:52:36 +0200

    Add test coverage report

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,11 @@
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/features/"
+end
+
 # Simple assert function
 def assert(condition, message = "Unknown reason")
   if condition
